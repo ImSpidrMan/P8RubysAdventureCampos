b8164a3d4a234893dc15815600f7ec92b46fd8c7
Gerardo Campos <751033@student.dusd.net>
2023-11-09 15:20:37 -0800

    Health
    Started on the collectible strawberry that gives ruby health

--- RubysAdventureCampos/Assets/Scripts/RubyController.cs
+++ RubysAdventureCampos/Assets/Scripts/RubyController.cs
@@ -6,6 +6,13 @@ using UnityEngine.Rendering.Universal;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -14,6 +21,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -26,9 +34,15 @@ public class NewBehaviourScript : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
