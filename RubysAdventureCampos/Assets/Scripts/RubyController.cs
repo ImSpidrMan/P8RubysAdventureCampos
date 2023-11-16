8a37a13642c68f911067c648f3d8a95a57d88526
Gerardo Campos <751033@student.dusd.net>
2023-11-16 15:25:53 -0800

    I finished sprite animations
    I have finished my sprite animations or the bot and for ruby.

--- RubysAdventureCampos/Assets/Scripts/RubyController.cs
+++ RubysAdventureCampos/Assets/Scripts/RubyController.cs
@@ -11,24 +11,27 @@ public class RubyController : MonoBehaviour
 
     public int maxHealth = 5;
 
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
     bool isInvincible;
     float invincibleTimer;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 5;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -36,6 +39,17 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2 (horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if(isInvincible)
         {
@@ -60,6 +74,8 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
